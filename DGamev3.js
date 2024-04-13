f75d4ce9b1cfe607623d72e012ed6a8afc03dfdf
damianhaq <damianhaq@gmail.com>
2024-04-13 21:01:38 +0200

    player class

--- DGamev3.js
+++ DGamev3.js
@@ -1,21 +1,98 @@
 // CLASSESS
 
+import { DGame } from "./DGamev2.js";
+
 /*
 Sprite to jest obiekt który może być interaktywny 
 1. nieporuszalne / poruszalne
 2. tekstura / animacja
 
-TODO: rysowanie, animowanie, pos/vel/acc, addForce, kolizja
+TODO:  pos/vel/acc, addForce, kolizja
 */
+
+export class Game {
+  constructor() {
+    this.canvas = null;
+    this.ctx = null;
+    this.scaleFactor = 1;
+    this.isDebug = true;
+
+    this.mouse = {
+      click: false,
+      x: false,
+      y: false,
+    };
+
+    this.keys = {
+      key: [],
+    };
+
+    this.camera = {
+      x: 0,
+      y: 0,
+    };
+  }
+  setCamera(x, y) {
+    this.camera.x = x - this.canvas.width / (2 * this.scaleFactor);
+    this.camera.y = y - this.canvas.height / (2 * this.scaleFactor);
+  }
+
+  init(canvasID, canvasWidth, canvasHeight, scaleFactor) {
+    this.canvas = document.querySelector(`#${canvasID}`);
+    this.ctx = this.canvas.getContext("2d");
+    this.scaleFactor = scaleFactor;
+
+    this.canvas.width = canvasWidth;
+    this.canvas.height = canvasHeight;
+    this.canvas.style.border = "1px solid black";
+    this.canvas.setAttribute("tabindex", 0);
+
+    this.canvas.addEventListener("mousedown", (ev) => {
+      this.mouse.click = true;
+    });
+    this.canvas.addEventListener("mouseup", (ev) => {
+      this.mouse.click = false;
+    });
+    this.canvas.addEventListener("mousemove", (ev) => {
+      this.mouse.x = Math.round(ev.offsetX / this.scaleFactor);
+      this.mouse.y = Math.round(ev.offsetY / this.scaleFactor);
+    });
+
+    this.canvas.addEventListener("keydown", (ev) => {
+      if (!this.keys.key[ev.keyCode]) this.keys.key[ev.keyCode] = true;
+      // console.log(ev.keyCode);
+    });
+    this.canvas.addEventListener("keyup", (ev) => {
+      if (this.keys.key[ev.keyCode]) this.keys.key[ev.keyCode] = false;
+    });
+
+    this.ctx.scale(this.scaleFactor, this.scaleFactor);
+    this.ctx.imageSmoothingEnabled = false;
+
+    this.setCamera(0, 0);
+
+    // console.log(this);
+  }
+
+  clearRect() {
+    this.ctx.fillStyle = "#898989";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
 export class Sprite {
-  constructor(x, y, width, height, ctx) {
+  constructor(x, y, width, height, game) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.ctx = ctx;
+    this.game = game;
 
-    this.viewType = "unset";
+    this.isFlipX = false;
+    this.isFlipY = false;
+    this.animName = "unset";
+
+    this.viewType = "unset"; // unset, texture, anim
   }
 
   addTexture(fromX, fromY, fromWidth, fromHeight, image) {
@@ -26,8 +103,6 @@ export class Sprite {
       fromWidth,
       fromHeight,
       image,
-      isFlipX: false,
-      isFlipY: false,
       rotateDeg: 0,
       rotatePointX: 0,
       rotatePointY: 0,
@@ -41,6 +116,14 @@ export class Sprite {
           `${this.width}x${this.height}. ` +
           "Zalecane jest zmianę wymiarów sprita na wymiary tekstury."
       );
+    }
+  }
+
+  setCurrentAnim(name) {
+    if (this.viewType === "anim") {
+      this.anim.currFrame = 0;
+      this.anim.currFrameTime = 0;
+      this.animName = name;
     }
   }
 
@@ -60,13 +143,11 @@ export class Sprite {
       currFrame: 0,
       frameTime: 100,
       currFrameTime: 0,
-      isFlipX: false,
-      isFlipY: false,
       rotateDeg: 0,
       rotatePointX: 0,
       rotatePointY: 0,
-      name: name,
     };
+    this.setCurrentAnim(name);
 
     if (fromWidth !== this.width || fromHeight !== this.height) {
       console.warn(
@@ -79,7 +160,7 @@ export class Sprite {
     }
   }
 
-  draw(deltaTime, animName = "") {
+  draw(deltaTime) {
     if (this.viewType === "texture") {
       drawImagePartWithTransform(
         this.texture.image,
@@ -87,52 +168,280 @@ export class Sprite {
         this.texture.fromY,
         this.texture.fromWidth,
         this.texture.fromHeight,
-        this.x,
-        this.y,
+        this.x - this.game.camera.x,
+        this.y - this.game.camera.y,
         this.texture.fromWidth,
         this.texture.fromHeight,
-        this.texture.isFlipX,
-        this.texture.isFlipY,
+        this.isFlipX,
+        this.isFlipY,
         this.texture.rotateDeg,
         this.texture.rotatePointX,
         this.texture.rotatePointY,
-        this.ctx,
+        this.game.ctx,
         0,
         0,
         true
       );
     } else if (this.viewType === "anim") {
-      this.anim[animName].currFrameTime += deltaTime;
-      if (this.anim[animName].currFrameTime >= this.anim[animName].frameTime) {
-        this.anim[animName].currFrameTime -= this.anim[animName].frameTime;
-        this.anim[animName].currFrame =
-          (this.anim[animName].currFrame + 1) % this.anim[animName].frames;
+      // animation logic
+      this.anim[this.animName].currFrameTime += deltaTime;
+      if (
+        this.anim[this.animName].currFrameTime >=
+        this.anim[this.animName].frameTime
+      ) {
+        this.anim[this.animName].currFrameTime -=
+          this.anim[this.animName].frameTime;
+        this.anim[this.animName].currFrame =
+          (this.anim[this.animName].currFrame + 1) %
+          this.anim[this.animName].frames;
       }
+
       drawImagePartWithTransform(
-        this.anim[animName].image,
-        this.anim[animName].fromX +
-          this.anim[animName].fromWidth * this.anim[animName].currFrame,
-        this.anim[animName].fromY,
-        this.anim[animName].fromWidth,
-        this.anim[animName].fromHeight,
-        this.x,
-        this.y,
-        this.anim[animName].fromWidth,
-        this.anim[animName].fromHeight,
-        this.anim[animName].isFlipX,
-        this.anim[animName].isFlipY,
-        this.anim[animName].rotateDeg,
-        this.anim[animName].rotatePointX,
-        this.anim[animName].rotatePointY,
-        this.ctx,
+        this.anim[this.animName].image,
+        this.anim[this.animName].fromX +
+          this.anim[this.animName].fromWidth *
+            this.anim[this.animName].currFrame,
+        this.anim[this.animName].fromY,
+        this.anim[this.animName].fromWidth,
+        this.anim[this.animName].fromHeight,
+        this.x - this.game.camera.x,
+        this.y - this.game.camera.y,
+        this.anim[this.animName].fromWidth,
+        this.anim[this.animName].fromHeight,
+        this.isFlipX,
+        this.isFlipY,
+        this.anim[this.animName].rotateDeg,
+        this.anim[this.animName].rotatePointX,
+        this.anim[this.animName].rotatePointY,
+        this.game.ctx,
         0,
         0,
         true
       );
     }
-    this.ctx.beginPath();
-    this.ctx.rect(this.x, this.y, this.width, this.height);
-    this.ctx.stroke();
+    // hitbox
+    this.game.ctx.beginPath();
+    this.game.ctx.rect(
+      this.x - this.game.camera.x,
+      this.y - this.game.camera.y,
+      this.width,
+      this.height
+    );
+    this.game.ctx.stroke();
+
+    // console.log(this.x, this.y);
+  }
+}
+
+export class Tiled {
+  constructor(game) {
+    this.game = game;
+  }
+  getTilePosFromSpritesheet(
+    id,
+    tilesetsColumns,
+    tilesetsTileWidth,
+    tilkesetsTileHeight
+  ) {
+    // TODO: dodać wyszukiwanie tileset po nazwie "name":"spritesheet",
+
+    const row = Math.floor((id - 1) / tilesetsColumns);
+    const column = id - 1 - row * tilesetsColumns;
+
+    return { x: column * tilesetsTileWidth, y: row * tilkesetsTileHeight };
+  }
+
+  get2dPosFrom1dArray(index, columns) {
+    const x = index % columns;
+    const y = Math.floor(index / columns);
+    return { x, y };
+  }
+
+  drawChunk(chunk, tileset, image) {
+    // this function draw chunk in correct position => chunk.x and chunk.y,
+    // so you dont have to specify where to draw this
+
+    // console.log(this.game.camera.x, this.game.camera.y);
+    // chunk position
+    const chunkX = chunk.x * tileset.tilewidth;
+    const chunkY = chunk.y * tileset.tileheight;
+
+    for (let i = 0; i < chunk.data.length; i++) {
+      const row = Math.floor(i / chunk.width);
+      const column = i - row * chunk.width;
+
+      if (chunk.data[i] !== 0) {
+        const tilePos = this.getTilePosFromSpritesheet(
+          chunk.data[i],
+          tileset.columns,
+          tileset.tilewidth,
+          tileset.tileheight
+        );
+
+        drawImagePartWithTransform(
+          image,
+          tilePos.x,
+          tilePos.y,
+          tileset.tilewidth,
+          tileset.tileheight,
+          0 + chunkX + column * tileset.tilewidth,
+          0 + chunkY + row * tileset.tileheight,
+          tileset.tilewidth,
+          tileset.tileheight,
+          false,
+          false,
+          0,
+          0,
+          0,
+          this.game.ctx,
+          this.game.camera.x,
+          this.game.camera.y,
+          false
+        );
+      }
+    }
+  }
+
+  drawLayer(layerName, layers, tileset, image) {
+    const layer = layers.filter((el) => el.name === layerName)[0];
+
+    for (let i = 0; i < layer.chunks.length; i++) {
+      this.drawChunk(layer.chunks[i], tileset, image);
+    }
+  }
+
+  getChunkIndex(myX, myY, layerIndex, jsonData) {
+    const tileX = Math.floor(myX / 16);
+    const tileY = Math.floor(myY / 16);
+
+    // TODO: on przeszukuje pierwszy layer, jeśli był by mniejszy (nie wiem czy to możliwe) to zwróci błędny chunk
+    // TODO: 16 jest hardcoded a nie powinno
+    const index = jsonData.layers[layerIndex].chunks.findIndex(
+      (el) =>
+        tileX >= el.x &&
+        tileX <= el.x + el.width &&
+        tileY >= el.y &&
+        tileY <= el.y + el.height
+    );
+
+    return index;
+    // console.log(index);
+  }
+}
+
+// Vector
+
+export class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  set(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(x, y) {
+    this.x += x;
+    this.y += y;
+  }
+
+  sub(x, y) {
+    this.x -= x;
+    this.y -= y;
+  }
+
+  mul(x, y) {
+    this.x *= x;
+    this.y *= y;
+  }
+
+  div(x, y) {
+    this.x /= x;
+    this.y /= y;
+  }
+
+  getLen() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  normalize() {
+    const len = this.getLen();
+    if (len !== 0) {
+      this.x /= len;
+      this.y /= len;
+    }
+  }
+
+  limit(max) {
+    if (this.getLen() > max) {
+      this.normalize();
+      this.mul(max);
+    }
+  }
+
+  getClone() {
+    return new Vector(this.x, this.y);
+  }
+
+  setMag(mag) {
+    this.normalize();
+    this.mul(mag);
+  }
+
+  getDistance(v2) {
+    const dx = this.x - v2.x;
+    const dy = this.y - v2.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  getCopy() {
+    return new Vector(this.x, this.y);
+  }
+
+  getMag() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  getHeading() {
+    return Math.atan2(this.y, this.x);
+  }
+
+  setAngleDeg(angle) {
+    const rad = (angle * Math.PI) / 180;
+    this.set(Math.cos(rad), Math.sin(rad));
+  }
+
+  setAngleRad(angle) {
+    const rad = angle;
+    this.set(Math.cos(rad), Math.sin(rad));
+  }
+
+  getAngleDeg() {
+    const rad = Math.atan2(this.y, this.x);
+    return (rad * 180) / Math.PI;
+  }
+
+  getAngleRad() {
+    const rad = Math.atan2(this.y, this.x);
+    return rad;
+  }
+
+  rotate(angle) {
+    const rad = (angle * Math.PI) / 180;
+    const x = this.x * Math.cos(rad) - this.y * Math.sin(rad);
+    const y = this.x * Math.sin(rad) + this.y * Math.cos(rad);
+    this.set(x, y);
+  }
+
+  static randomUnitVector() {
+    const random1 = Math.random() * 2 - 1;
+    const random2 = Math.random() * 2 - 1;
+    const magnitude = Math.sqrt(random1 ** 2 + random2 ** 2);
+    const unitX = random1 / magnitude;
+    const unitY = random2 / magnitude;
+    return new Vector(unitX, unitY);
   }
 }
 
--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 import { DGame } from "./DGamev2.js";
-import { Sprite, drawImagePartWithTransform } from "./DGamev3.js";
+import {
+  Game,
+  Sprite,
+  Tiled,
+  Vector,
+  drawImagePartWithTransform,
+} from "./DGamev3.js";
 import { spriteSheetData } from "./bigSpritev7data.js";
 import jsonData from "./gamev9.json" assert { type: "json" };
 
-DGame.init("canvas", 1600, 800, 2);
+const game = new Game();
+game.init("canvas", 1600, 800, 2);
+
+const tiled = new Tiled(game);
 
 const bigSpritev7 = new Image();
 bigSpritev7.src = "BigSpritev7.png";
 
-const newPlayer = new Sprite(10, 10, 16, 22, DGame.ctx);
+class Player extends Sprite {
+  constructor(x, y, width, height, game) {
+    super(x, y, width, height, game);
+
+    this.x = x;
+    this.y = y;
+    this.vel = new Vector(0, 0);
+    this.acc = new Vector(0, 0);
+  }
+
+  update() {
+    let normVel = new Vector(0, 0);
+    let isMoving = false;
+    if (game.keys.key[65]) {
+      normVel.x = -1;
+      this.isFlipX = true;
+      isMoving = true;
+    } else if (game.keys.key[68]) {
+      normVel.x = 1;
+      this.isFlipX = false;
+      isMoving = true;
+    }
+    if (game.keys.key[87]) {
+      normVel.y = -1;
+      isMoving = true;
+    } else if (game.keys.key[83]) {
+      normVel.y = 1;
+      isMoving = true;
+    }
+
+    normVel.normalize();
+    this.x += normVel.x;
+    this.y += normVel.y;
+
+    // reset vel to 0 if no key is pressed
+    this.setCurrentAnim(isMoving ? "run" : "idle");
+  }
+}
+
+const newPlayer = new Player(10, 10, 16, 22, game);
+
 // newPlayer.addTexture(
 //   spriteSheetData.purpleKnight.idle.x,
 //   spriteSheetData.purpleKnight.idle.y,
@@ -35,210 +84,166 @@ newPlayer.addAnim(
   bigSpritev7
 );
 
-let spriteOrc = DGame.sprite.createCircle(50, -100, 25);
+// let skeletonSprite = DGame.sprite.createRect(150, -100, 20, 20);
+// const {
+//   x: sx,
+//   y: sy,
+//   w: sw,
+//   h: sh,
+//   frames: sf,
+// } = spriteSheetData.skeleton.idle;
+// skeletonSprite = DGame.sprite.addAnim(
+//   sx,
+//   sy,
+//   sw,
+//   sh,
+//   sf,
+//   bigSpritev7,
+//   skeletonSprite
+// );
 
-const {
-  x: ox,
-  y: oy,
-  w: ow,
-  h: oh,
-  frames: of,
-} = spriteSheetData.orcInMask.idle;
+// const player = {
+//   position: DGame.vector.create(100, 100),
+//   // velocity is like "next step"
+//   velocity: DGame.vector.create(0, 0),
+//   acc: DGame.vector.create(0, 0),
 
-spriteOrc = DGame.sprite.addAnim(ox, oy, ow, oh, of, bigSpritev7, spriteOrc);
+//   draw: function (deltaTime) {
+//     skeletonSprite.x = this.position.x;
+//     skeletonSprite.y = this.position.y;
 
-// console.log("sprite Orc", spriteOrc);
+//     DGame.sprite.draw(skeletonSprite, deltaTime);
 
-let skeletonSprite = DGame.sprite.createRect(150, -100, 20, 20);
-const {
-  x: sx,
-  y: sy,
-  w: sw,
-  h: sh,
-  frames: sf,
-} = spriteSheetData.skeleton.idle;
-skeletonSprite = DGame.sprite.addAnim(
-  sx,
-  sy,
-  sw,
-  sh,
-  sf,
-  bigSpritev7,
-  skeletonSprite
-);
+//     DGame.draw.line(
+//       this.position.x,
+//       this.position.y,
+//       this.velocity.x + this.position.x,
+//       this.velocity.y + this.position.y
+//     );
+//   },
 
-let elfSprite = DGame.sprite.createCircle(150, 100, 8);
-elfSprite = DGame.sprite.addImage(
-  spriteSheetData.elfM.idle.x,
-  spriteSheetData.elfM.idle.y,
-  spriteSheetData.elfM.idle.w,
-  spriteSheetData.elfM.idle.h,
-  bigSpritev7,
-  elfSprite
-);
-elfSprite.image.isFlipX = false;
+//   applyForce: function (vector) {
+//     this.acc = DGame.vector.add(this.acc, vector);
+//   },
 
-// console.log(elfSprite);
+//   update: function () {
+//     // "Motion 101"
 
-const player = {
-  position: DGame.vector.create(100, 100),
-  // velocity is like "next step"
-  velocity: DGame.vector.create(0, 0),
-  acc: DGame.vector.create(0, 0),
+//     // const mousePos = DGame.vector.create(
+//     //   DGame.mouse.x + DGame.camera.x,
+//     //   DGame.mouse.y + DGame.camera.y
+//     // );
+//     // const vSelfToMouse = DGame.vector.sub(mousePos, this.position);
+//     // const unitVSelfToMouse = DGame.vector.unitVector(vSelfToMouse);
+//     // const slowVSelfToMouse = DGame.vector.mult(vSelfToMouse, 0.0001);
 
-  draw: function (deltaTime) {
-    skeletonSprite.x = this.position.x;
-    skeletonSprite.y = this.position.y;
+//     this.velocity = DGame.vector.add(this.velocity, this.acc);
+//     this.position = DGame.vector.add(this.position, this.velocity);
+//     this.acc = DGame.vector.create(0, 0);
+//     this.velocity = DGame.vector.create(0, 0);
 
-    DGame.sprite.draw(skeletonSprite, deltaTime);
+//     this.movement();
+//   },
 
-    DGame.draw.line(
-      this.position.x,
-      this.position.y,
-      this.velocity.x + this.position.x,
-      this.velocity.y + this.position.y
-    );
-  },
+//   movement: function () {
+//     let vector = DGame.vector.create(0, 0);
+//     if (
+//       DGame.keys.key[65]
+//       // DGame.physics.isAABBCollision(this.position.x, this.position.y, 16,16,)
+//     ) {
+//       vector.x = -1;
+//       skeletonSprite.anim.isFlipX = true;
+//     } else if (DGame.keys.key[68]) {
+//       vector.x = 1;
+//       skeletonSprite.anim.isFlipX = false;
+//     }
+//     if (DGame.keys.key[87]) {
+//       vector.y = -1;
+//     } else if (DGame.keys.key[83]) {
+//       vector.y = 1;
+//     }
 
-  applyForce: function (vector) {
-    this.acc = DGame.vector.add(this.acc, vector);
-  },
+//     this.velocity = DGame.vector.add(
+//       this.velocity,
+//       DGame.vector.unitVector(vector)
+//     );
+//     // console.log(this.velocity);
+//   },
+// };
 
-  update: function () {
-    // "Motion 101"
-
-    // const mousePos = DGame.vector.create(
-    //   DGame.mouse.x + DGame.camera.x,
-    //   DGame.mouse.y + DGame.camera.y
-    // );
-    // const vSelfToMouse = DGame.vector.sub(mousePos, this.position);
-    // const unitVSelfToMouse = DGame.vector.unitVector(vSelfToMouse);
-    // const slowVSelfToMouse = DGame.vector.mult(vSelfToMouse, 0.0001);
-
-    this.velocity = DGame.vector.add(this.velocity, this.acc);
-    this.position = DGame.vector.add(this.position, this.velocity);
-    this.acc = DGame.vector.create(0, 0);
-    this.velocity = DGame.vector.create(0, 0);
-
-    this.movement();
-  },
-
-  movement: function () {
-    let vector = DGame.vector.create(0, 0);
-    if (
-      DGame.keys.key[65]
-      // DGame.physics.isAABBCollision(this.position.x, this.position.y, 16,16,)
-    ) {
-      vector.x = -1;
-      skeletonSprite.anim.isFlipX = true;
-    } else if (DGame.keys.key[68]) {
-      vector.x = 1;
-      skeletonSprite.anim.isFlipX = false;
-    }
-    if (DGame.keys.key[87]) {
-      vector.y = -1;
-    } else if (DGame.keys.key[83]) {
-      vector.y = 1;
-    }
-
-    this.velocity = DGame.vector.add(
-      this.velocity,
-      DGame.vector.unitVector(vector)
-    );
-    // console.log(this.velocity);
-  },
-};
-
-// console.log("player", player);
-
-function egdes() {
-  if (player.position.y >= 200) {
-    player.position.y = 200;
-    player.velocity.y *= -1;
-  }
-  if (player.position.x >= 400) {
-    player.position.x = 400;
-    player.velocity.x *= -1;
-  }
-  // if (player.position.y >= 200) {
-  //   player.position.y = 200;
-  //   player.velocity.y *= -1;
-  // }
-  // if (player.position.y >= 200) {
-  //   player.position.y = 200;
-  //   player.velocity.y *= -1;
-  // }
-}
+// function egdes() {
+//   if (player.position.y >= 200) {
+//     player.position.y = 200;
+//     player.velocity.y *= -1;
+//   }
+//   if (player.position.x >= 400) {
+//     player.position.x = 400;
+//     player.velocity.x *= -1;
+//   }
+//   // if (player.position.y >= 200) {
+//   //   player.position.y = 200;
+//   //   player.velocity.y *= -1;
+//   // }
+//   // if (player.position.y >= 200) {
+//   //   player.position.y = 200;
+//   //   player.velocity.y *= -1;
+//   // }
+// }
 
 function update(deltaTime) {
   // player.applyForce(DGame.vector.create(0, 0.01));
   // player.applyForce(DGame.vector.create(0.01, 0));
-  player.update();
-
-  DGame.camera.set(player.position.x, player.position.y);
+  newPlayer.update();
+  // game.camera.set(player.position.x, player.position.y);
+  game.setCamera(newPlayer.x, newPlayer.y);
 }
 
 let rotate = 0;
 function draw(deltaTime) {
-  DGame.clearRect();
+  game.clearRect();
 
-  DGame.tiled.drawLayer(
+  tiled.drawLayer(
     "background",
     jsonData.layers,
     jsonData.tilesets[0],
     bigSpritev7
   );
-  DGame.tiled.drawLayer(
-    "walls",
-    jsonData.layers,
-    jsonData.tilesets[0],
-    bigSpritev7
-  );
-  DGame.tiled.drawLayer(
-    "chests",
-    jsonData.layers,
-    jsonData.tilesets[0],
-    bigSpritev7
-  );
-  player.draw(deltaTime);
-  // DGame.sprite.draw(testSprite1);
-  // DGame.sprite.draw(testSprite2);
-  DGame.sprite.draw(spriteOrc, deltaTime);
-
-  DGame.sprite.draw(elfSprite, deltaTime);
-  DGame.tiled.drawLayer(
+  tiled.drawLayer("walls", jsonData.layers, jsonData.tilesets[0], bigSpritev7);
+  tiled.drawLayer("chests", jsonData.layers, jsonData.tilesets[0], bigSpritev7);
+  tiled.drawLayer(
     "foreground",
     jsonData.layers,
     jsonData.tilesets[0],
     bigSpritev7
   );
 
+  // player.draw(deltaTime);
+
   // test
 
-  // newPlayer.x = rotate;
-  newPlayer.draw(deltaTime, "run");
+  newPlayer.draw(deltaTime);
   // newPlayer.anim.rotateDeg = rotate;
   rotate++;
   if (rotate > 360) rotate = 0;
 
   // collision WIP
-  const chunkIndex = DGame.tiled.getChunkIndex(
-    player.position.x,
-    player.position.y,
-    2,
-    jsonData
-  );
+  // const chunkIndex = DGame.tiled.getChunkIndex(
+  //   player.position.x,
+  //   player.position.y,
+  //   2,
+  //   jsonData
+  // );
 
-  if (currentChunkIndex !== chunkIndex) {
-    currentChunkIndex = chunkIndex;
-    collidable.length = 0;
-    addTilesToCollidable(2, currentChunkIndex);
-    // console.log("collidable", collidable);
-  }
+  // if (currentChunkIndex !== chunkIndex) {
+  //   currentChunkIndex = chunkIndex;
+  //   collidable.length = 0;
+  //   addTilesToCollidable(2, currentChunkIndex);
+  //   // console.log("collidable", collidable);
+  // }
 
   // draw collidable
 
-  collidable.forEach((el) => DGame.draw.rect(el.x, el.y, 16, 16));
+  // collidable.forEach((el) => DGame.draw.rect(el.x, el.y, 16, 16));
 }
 
 let currentChunkIndex = null;
